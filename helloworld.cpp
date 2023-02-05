6a4c4a2feca212d6bb0a02efd8e1df45091b6ca4
zath <zathacode@gmail.com>
2023-02-05 12:49:57 +0530

    updated helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
     cout << "Hello world" << endl; // printing only one string and adding a new line after it using endl (we can also use "\n" for doing the same thing instead of endl)
+    // some people prefer "\n" because it is slightly faster accoring to them
     cout << "Hello world" << " second string"; // printing second string in the same line
 }
