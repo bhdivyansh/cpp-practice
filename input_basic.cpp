d15e3044d8c3a1732f480c610edab79536e9e1ac
zath <zathacode@gmail.com>
2023-02-05 12:56:03 +0530

    added basic input file

---
+++ input_basic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x; // Taking input for one variable
+
+    cout<<"The input given (stored in x) is "<<x;
+
+    int a,b;
+    cin >> a >> b; // Taking input for two variables
+
+    return 0;
+}
