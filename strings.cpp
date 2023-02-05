40a4294cf29a8e680629b3e4ccf7ad036280edc8
zath <zathacode@gmail.com>
2023-02-05 22:10:20 +0530

    Added strings.cpp

---
+++ strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+// #include <bits/stdc++.h> // We can alternatively just include this (instead of the other two)
+using namespace std;
+int main(){
+    string x = "mystring";
+    cout << x << endl;
+
+    string firstName = "John";
+    string lastName = "Doe";
+    string fullName = firstName + " " + lastName; // example of concatenation in strings
+
+    cout << fullName;
+    return 0;
+}
