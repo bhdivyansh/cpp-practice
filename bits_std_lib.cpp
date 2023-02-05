78a1e36766418adae1044811ea5d665b8a26d9ed
zath <zathacode@gmail.com>
2023-02-05 13:13:28 +0530

    added bits/stdc++ notes

---
+++ bits_std_lib.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h> // Include this and the code runs smoothly, no need to include anything else.
+
+//  Note: Including bits/stdc++.h takes negligible time to run as compared to the time taken by our program to run.
+using namespace std;
+int main(){
+    cout<<"hello";
+    return 0;
+}
